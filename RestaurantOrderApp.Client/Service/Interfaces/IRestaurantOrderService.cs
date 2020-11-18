49c99b691baa5dce2d16dfbf2213c10653a454fc
Anderson L Nichele <nichele.anderson@gmail.com>
2020-11-18 11:17:01 -0300

    Add console client app

---
+++ RestaurantOrderApp.Client/Service/Interfaces/IRestaurantOrderService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace RestaurantOrderApp.Client.Service.Interfaces
+{
+    public interface IRestaurantOrderService
+    {
+        Task<string> GetOrder(string inputOrder);
+    }
+}
